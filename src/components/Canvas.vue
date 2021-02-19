e2daf586af703452d219f10d53a400c75a8b4260
bdavies01 <bertdaviezz@gmail.com>
2021-02-18 17:27:38 -0800

    Initialize Canvas.vue and deleted other starting components.

---
+++ src/components/Canvas.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="background">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Canvas",
+  props: {
+    msg: String
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Canvas msg="Message placeholder" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
+import Canvas from "@/components/Canvas.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Canvas
   }
 };
 </script>
