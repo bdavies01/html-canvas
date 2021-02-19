a89b0888882567c7ac8a84a46de78a7ebc76badb
bdavies01 <bertdaviezz@gmail.com>
2021-02-18 17:29:10 -0800

    Removed remaining helloworld comments.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="home">
     <Canvas msg="Message placeholder" />
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-//import HelloWorld from "@/components/HelloWorld.vue";
 import Canvas from "@/components/Canvas.vue";
 
 export default {
