2a04d7822aaa1c17c6d7268c0193643f6767fe9b
bdavies01 <bertdaviezz@gmail.com>
2021-02-19 17:26:00 -0800

    moved js code to canvas.vue

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="background">
-    <h1>{{ msg }}</h1>
+    <h1>{{ mesage }}</h1>
     <canvas id="canvas_element" width="600" height="600"> </canvas>
   </div>
 </template>
@@ -8,8 +8,45 @@
 <script>
 export default {
   name: "Canvas",
+  data: function() {
+    return {
+      mesage: "woah"
+    };
+  },
   props: {
     msg: String
+  },
+  mounted: function() {
+    const canvas = document.getElementById("canvas_element");
+    const ctx = canvas.getContext("2d");
+
+    draw_rectangle(ctx);
+    draw_img(ctx, "assets/clown.jpg", 300, 100);
+    draw_img(ctx, "assets/hand.jpg", 100, 300);
+    draw_circle(ctx);
   }
 };
+
+function draw_rectangle(canv2d) {
+  canv2d.fillStyle = "orange";
+  canv2d.fillRect(50, 50, 100, 175);
+}
+
+function draw_img(canv2d, imgsrc, x, y) {
+  const img_to_draw = new Image();
+  img_to_draw.src = imgsrc;
+  img_to_draw.onload = function() {
+    canv2d.drawImage(img_to_draw, x, y, 150, 150);
+  };
+}
+
+function draw_circle(canv2d) {
+  const centerX = 400;
+  const centerY = 400;
+  const radius = 50;
+  canv2d.beginPath();
+  canv2d.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+  canv2d.fillStyle = "red";
+  canv2d.fill();
+}
 </script>
