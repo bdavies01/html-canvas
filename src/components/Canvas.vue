569d5eca29e9cd306d8116ab7ae64039e6e92d35
bdavies01 <bertdaviezz@gmail.com>
2021-02-22 22:03:18 -0800

    changed design around more objects.

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="background">
     <h1>{{ message }}</h1>
-    <canvas id="canvas_element" width="600" height="600"> </canvas>
+    <canvas id="canvasElement" width="600" height="600"> </canvas>
   </div>
 </template>
 
@@ -11,41 +11,75 @@ export default {
   data: function() {
     return {
       message: "Default",
-      orangerect: {
-        x: 50,
-        y: 50,
-        w: 100,
-        l: 175
+      rectDict: {
+        orangeRect: {
+          name: "orange rectangle",
+          type: "shape",
+          x: 50,
+          y: 50,
+          w: 100,
+          l: 175
+        },
+        clown: {
+          name: "clown",
+          type: "img",
+          x: 300,
+          y: 100,
+          w: 150,
+          l: 150,
+          path: "assets/clown.jpg"
+        },
+        hand: {
+          name: "hand",
+          type: "img",
+          x: 100,
+          y: 300,
+          w: 150,
+          l: 150,
+          path: "assets/hand.jpg"
+        }
       },
-      clown: {
-        x: 300,
-        y: 100,
-        w: 150,
-        l: 150,
-        path: "assets/clown.jpg"
-      },
-      hand: {
-        x: 100,
-        y: 300,
-        w: 150,
-        l: 150,
-        path: "assets/hand.jpg"
-      },
-      circle: {
-        x: 400,
-        y: 400,
-        radius: 50
+      circleDict: {
+        circle: {
+          name: "circle",
+          x: 400,
+          y: 400,
+          radius: 50
+        }
       }
     };
   },
   mounted: function() {
-    const canvas = document.getElementById("canvas_element");
+    const canvas = document.getElementById("canvasElement");
     const ctx = canvas.getContext("2d");
 
-    draw_rectangle(ctx);
-    draw_img(ctx, this.clown);
-    draw_img(ctx, this.hand);
-    draw_circle(ctx, this.circle);
+    for (var ridx in this.rectDict) {
+      const rectangle = this.rectDict[ridx];
+      if (rectangle.type == "shape") {
+        ctx.fillStyle = "orange";
+        ctx.fillRect(rectangle.x, rectangle.y, rectangle.w, rectangle.l);
+      } else if (rectangle.type == "img") {
+        const imgToDraw = new Image();
+        imgToDraw.src = rectangle.path;
+        imgToDraw.onload = function() {
+          ctx.drawImage(
+            imgToDraw,
+            rectangle.x,
+            rectangle.y,
+            rectangle.w,
+            rectangle.l
+          ); //prettier wants this formatting
+        };
+      }
+    }
+
+    for (var cidx in this.circleDict) {
+      const circle = this.circleDict[cidx];
+      ctx.beginPath();
+      ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI, false);
+      ctx.fillStyle = "red";
+      ctx.fill();
+    }
 
     canvas.addEventListener("mousemove", e => {
       const rect = canvas.getBoundingClientRect();
@@ -53,52 +87,37 @@ export default {
         x: e.clientX - rect.left,
         y: e.clientY - rect.top
       };
-      if (isIntersectRect(pos, this.orangerect)) {
-        this.message = "rectangle!";
-      } else if (isIntersectRect(pos, this.clown)) {
-        this.message = "clown!";
-      } else if (isIntersectRect(pos, this.hand)) {
-        this.message = "hand!";
-      } else if (isIntersectCircle(pos, this.circle)) {
-        this.message = "circle!";
-      } else {
-        this.message = "Default";
+      for (var ridx in this.rectDict) {
+        const rectangle = this.rectDict[ridx];
+        isIntersectRect(this, pos, rectangle);
+      }
+      for (var cidx in this.circleDict) {
+        const circle = this.circleDict[cidx];
+        isIntersectCircle(this, pos, circle);
       }
     });
   }
 };
 
-function draw_rectangle(canv2d) {
-  canv2d.fillStyle = "orange";
-  canv2d.fillRect(50, 50, 100, 175);
-}
-
-function draw_img(canv2d, img) {
-  const img_to_draw = new Image();
-  img_to_draw.src = img.path;
-  img_to_draw.onload = function() {
-    canv2d.drawImage(img_to_draw, img.x, img.y, img.w, img.l);
-  };
-}
-
-function draw_circle(canv2d, circle) {
-  canv2d.beginPath();
-  canv2d.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI, false);
-  canv2d.fillStyle = "red";
-  canv2d.fill();
-}
-
-function isIntersectCircle(point, circle) {
+function isIntersectCircle(obj, point, circle) {
   var dist = Math.sqrt((point.x - circle.x) ** 2 + (point.y - circle.y) ** 2);
-  return dist < circle.radius;
+  if (dist < circle.radius) {
+    obj.message = circle.name;
+  } else if (obj.message == "Default" || obj.message == circle.name) {
+    obj.message = "Default";
+  }
 }
 
-function isIntersectRect(point, rect) {
-  return (
+function isIntersectRect(obj, point, rect) {
+  if (
     point.x >= rect.x &&
     point.x <= rect.x + rect.w &&
     point.y >= rect.y &&
     point.y <= rect.y + rect.l
-  );
+  ) {
+    obj.message = rect.name;
+  } else if (obj.message == "Default" || obj.message == rect.name) {
+    obj.message = "Default";
+  }
 }
 </script>
